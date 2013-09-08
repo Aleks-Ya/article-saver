c08f876d06a643536d2c482799f971d382e55095
Aleksey Yablokov <ya_al@bk.ru>
2013-09-08 08:49:07 +0400

    fix добавил обработку некорректных ссылок на статьи: MalformedURL и пустые

--- src/ru/yaal/project/habrahabr/saver/FileParameters.java
+++ src/ru/yaal/project/habrahabr/saver/FileParameters.java
@@ -6,12 +6,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 import static java.lang.String.format;
 
@@ -26,6 +28,7 @@ public class FileParameters implements IParameters {
     public static final String DEFAULT_ARTICLES_FILE_NAME = "ArticleUrls.txt";
     public static final String TARGET_FOLDER_PROPERTY_NAME = "targetFolder";
     private static final Logger LOG = Logger.getLogger(FileParameters.class);
+    private static final Pattern EMPTY_LINE_PATTERN = Pattern.compile("^\\s*$");
     private Path targetFolder;
     private List<Article> articles;
 
@@ -47,7 +50,13 @@ public class FileParameters implements IParameters {
         BufferedReader in = new BufferedReader(new FileReader(articlesFile));
         String line;
         while ((line = in.readLine()) != null) {
-            result.add(new Article(new URL(line)));
+            if (!EMPTY_LINE_PATTERN.matcher(line).matches()) {
+                try {
+                    result.add(new Article(new URL(line)));
+                } catch (MalformedURLException e) {
+                    LOG.warn(format("Пропускаю некорректную ссылку на статью: %s", line));
+                }
+            }
         }
         return result;
     }
